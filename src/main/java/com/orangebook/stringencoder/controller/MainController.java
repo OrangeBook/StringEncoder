7d648a0f7eb7e1b6bcd3159af5123768295ee49b
Lukianchenko <qlukianchenkop@gmail.com>
2020-11-01 21:41:43 +0200

    add encoding logic

--- src/main/java/com/orangebook/stringencoder/controller/MainController.java
+++ src/main/java/com/orangebook/stringencoder/controller/MainController.java
@@ -1,12 +1,15 @@
 package com.orangebook.stringencoder.controller;
 
+import java.util.Base64;
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
-
 
   @GetMapping("/main")
   public String main() {
@@ -19,7 +22,15 @@ public class MainController {
   }
 
   @PostMapping("/encoder/action")
-  public String action() {
-    return "encoder";
+  public ModelAndView action(@ModelAttribute("decoded_text") String attribute) {
+
+    System.out.println(attribute);
+
+    String encodedText = Base64.getEncoder().encodeToString(attribute.getBytes());
+    ModelAndView modelAndView = new ModelAndView();
+    modelAndView.addObject("encoded_text", encodedText);
+
+    modelAndView.setViewName("encoder");
+    return modelAndView;
   }
 }
