eabdc4ef7ab8f602af28c9ac5e2cd9e60b7a66f7
Lukianchenko <qlukianchenkop@gmail.com>
2020-11-03 16:47:20 +0200

    add decoding logic

--- src/main/java/com/orangebook/stringencoder/controller/MainController.java
+++ src/main/java/com/orangebook/stringencoder/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.orangebook.stringencoder.controller;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Base64;
 import java.util.Map;
 import org.springframework.stereotype.Controller;
@@ -21,14 +22,34 @@ public class MainController {
     return "encoder";
   }
 
-  @PostMapping("/encoder/action")
-  public ModelAndView action(@ModelAttribute("decoded_text") String attribute) {
+  @PostMapping("/encoder/encode")
+  public ModelAndView encode(@ModelAttribute("decoded_text") String attribute) {
 
     System.out.println(attribute);
 
     String encodedText = Base64.getEncoder().encodeToString(attribute.getBytes());
     ModelAndView modelAndView = new ModelAndView();
     modelAndView.addObject("encoded_text", encodedText);
+
+    modelAndView.setViewName("encoder");
+    return modelAndView;
+  }
+
+  @PostMapping("/encoder/decode")
+  public ModelAndView decode(@ModelAttribute("encoded_text") String attribute) {
+
+    System.out.println(attribute);
+
+    byte[] bytes =  Base64.getDecoder().decode(attribute.getBytes());
+    String decodedText = null;
+    try {
+      decodedText = new String(bytes, "UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      System.out.println("Encoding problems");
+    }
+
+    ModelAndView modelAndView = new ModelAndView();
+    modelAndView.addObject("decoded_text", decodedText);
 
     modelAndView.setViewName("encoder");
     return modelAndView;
